6524ef59d31c19b1f67e7374949899ee9ca13da8
Carlos Garcia <dancingevilgenius@gmail.com>
2025-11-19 05:05:41 -0600

    Added code to initialize QWIIC Motor Driver. Not integrated.

--- ESP32/ESP32_PID_Line_Follower_PS3/ESP32_PID_Line_Follower_PS3.ino
+++ ESP32/ESP32_PID_Line_Follower_PS3/ESP32_PID_Line_Follower_PS3.ino
@@ -19,6 +19,12 @@
  * Author: Bot Reboot
  */
 
+#include <Arduino.h>
+#include <stdint.h>
+#include "SCMD.h"         // Serial Controlled Motor Driver
+#include "SCMD_config.h"  // Contains #defines for common SCMD register names and values
+#include "Wire.h"         // For I2C/QWIIC/STEMMA Communication.
+
 
 
 #define LED_BUILTIN 2 // works with ESP32 DEV board, Acebott ESP32-Max
@@ -46,6 +52,14 @@
 #define SENSOR_POS_100P   7000
 
 int sensorPosition = SENSOR_POS_CENTER; // A single value used represent line relative to sensor array.
+
+
+// For QWIIC Motor Driver
+#define DIR_FW  0
+#define DIR_RV  1
+#define LEFT_MOTOR 0
+#define RIGHT_MOTOR 1
+SCMD myMotorDriver; //This creates the main object of one motor driver and connected peripherals.
 
 
 
@@ -160,6 +174,14 @@ void setupReadouts(){
 }
 
 void setupMotors(){
+
+  setupQwiicMotorDriver();
+
+
+}
+
+// Legacy No plans for using this motor driver.
+void setupDRV8835MotorDriver(){
   pinMode(mode, OUTPUT);
   pinMode(aphase, OUTPUT);
   pinMode(aenbl, OUTPUT);
@@ -169,6 +191,7 @@ void setupMotors(){
   digitalWrite(mode,HIGH);  //one of the two control interfaces (simplified drive/brake operation)
 
 }
+
 
 void setupLineSensors(){
   // OSOYOO setup here
@@ -387,5 +410,51 @@ int getOsoyooSensorPosition(){
   }
 
 }
+
+void setupQwiicMotorDriver(){
+  //***** Configure the Motor Driver's Settings *****//
+  //  .commInter face is I2C_MODE 
+  myMotorDriver.settings.commInterface = I2C_MODE;
+
+  //  set address if I2C configuration selected with the config jumpers
+  myMotorDriver.settings.I2CAddress = 0x5D; //config pattern is "1000" (default) on board for address 0x5D
+
+  //  set chip select if SPI selected with the config jumpers
+  myMotorDriver.settings.chipSelectPin = 10;
+
+  //*****initialize the driver get wait for idle*****//
+  while ( myMotorDriver.begin() != 0xA9 ) //Wait until a valid ID word is returned
+  {
+    Serial.print("begin() return word ");
+    Serial.print(myMotorDriver.begin(), HEX);
+    Serial.print(" : ");
+    Serial.println( "ID mismatch, trying again" );    
+    delay(500);
+  }
+  Serial.println( "ID matches 0xA9" );
+
+  //  Check to make sure the driver is done looking for peripherals before beginning
+  Serial.print("Waiting for enumeration...");
+  while ( myMotorDriver.ready() == false );
+  Serial.println("Done.");
+  Serial.println();
+
+  //*****Set application settings and enable driver*****//
+
+  //Uncomment code for motor 0 inversion
+  //while( myMotorDriver.busy() );
+  //myMotorDriver.inversionMode(0, 1); //invert motor 0
+
+  //Uncomment code for motor 1 inversion
+  while ( myMotorDriver.busy() ); //Waits until the SCMD is available.
+  myMotorDriver.inversionMode(1, 1); //invert motor 1
+
+  while ( myMotorDriver.busy() )
+    ; // Do nothing until driver is available
+
+  myMotorDriver.enable(); //Enables the output driver hardware
+
+}
+
 
 
