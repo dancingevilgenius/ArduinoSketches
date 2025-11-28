a2f2d6b5e13166c7ee62eddeb5ce7ca3f38f10cd
Carlos Garcia <dancingevilgenius@gmail.com>
2025-11-28 06:48:21 -0600

    First checkin. Copy of HUSKYLENS_GetStarted.  Moved some code from loop to setup.

---
+++ Rev3/HUSKYLENS_DragRacer/HUSKYLENS_DragRacer.ino
@@ -0,0 +1,88 @@
+/***************************************************
+ HUSKYLENS An Easy-to-use AI Machine Vision Sensor
+ <https://www.dfrobot.com/product-1922.html>
+ 
+ ***************************************************
+ This example shows the basic function of library for HUSKYLENS via Serial.
+ 
+ Created 2020-03-13
+ By [Angelo qiao](Angelo.qiao@dfrobot.com)
+ 
+ GNU Lesser General Public License.
+ See <http://www.gnu.org/licenses/> for details.
+ All above must be included in any redistribution
+ ****************************************************/
+
+/***********Notice and Trouble shooting***************
+ 1.Connection and Diagram can be found here
+ <https://wiki.dfrobot.com/HUSKYLENS_V1.0_SKU_SEN0305_SEN0336#target_23>
+ 2.This code is tested on Arduino Uno, Leonardo, Mega boards.
+ ****************************************************/
+
+#include "HUSKYLENS.h"
+#include "SoftwareSerial.h"
+
+HUSKYLENS huskylens;
+SoftwareSerial mySerial(10, 11); // RX, TX      Inland RX:10 TX:11
+//HUSKYLENS green line >> Pin 10; blue line >> Pin 11
+void printResult(HUSKYLENSResult result);
+
+void setup() {
+    Serial.begin(115200);
+    mySerial.begin(9600);
+    Serial.println("setup() started -----------");
+
+    while (!huskylens.begin(mySerial))
+    {
+        Serial.println(F("Begin failed!"));
+        Serial.println(F("1.Please recheck the \"Protocol Type\" in HUSKYLENS (General Settings>>Protocol Type>>Serial 9600)"));
+        Serial.println(F("2.Please recheck the connection."));
+        delay(2000);
+    }
+
+    // if(!huskylens.isLearned()) {
+
+    //     Serial.println("Nothing learned, press learn button on HUSKYLENS to calibrate line! Exiting program.");
+    //     exit(1);
+    // }
+
+    while(!huskylens.request()) {
+        Serial.println(F("Fail to request data from HUSKYLENS, recheck the connection!"));
+        delay(2000);
+    }
+
+
+    HUSKYLENSResult result = huskylens.read();
+    if (result.command == COMMAND_RETURN_BLOCK){
+        Serial.println("Huskylens in RETURN_BLOCK mode. Change to Line Follow Mode. Exiting program");
+        exit(2);
+    }
+
+    while(!huskylens.available()) {
+        Serial.println(F("No block or arrow appears on the screen!"));
+        delay(1000);
+    }
+
+}
+
+void loop() {
+    //Serial.println(F("###########"));
+    while (huskylens.available())
+    {
+        HUSKYLENSResult result = huskylens.read();
+        printResult(result);
+        delay(1000);
+    }    
+}
+
+void printResult(HUSKYLENSResult result){
+    if (result.command == COMMAND_RETURN_BLOCK){
+        Serial.println(String()+F("Block:xCenter=")+result.xCenter+F(",yCenter=")+result.yCenter+F(",width=")+result.width+F(",height=")+result.height+F(",ID=")+result.ID);
+    }
+    else if (result.command == COMMAND_RETURN_ARROW){
+        Serial.println(String()+F("Arrow:xOrigin=")+result.xOrigin+F(",yOrigin=")+result.yOrigin+F(",xTarget=")+result.xTarget+F(",yTarget=")+result.yTarget+F(",ID=")+result.ID);
+    }
+    else{
+        Serial.println("Object unknown!");
+    }
+}
