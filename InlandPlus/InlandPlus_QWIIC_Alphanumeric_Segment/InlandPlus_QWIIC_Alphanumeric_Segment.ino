0a8319a3a12d29e76d65dba0a0abcb5a95bd2037
Carlos Garcia <dancingevilgenius@gmail.com>
2025-12-28 09:01:37 -0600

    Replaced hard coded letter segment codes with #defines.

--- InlandPlus/InlandPlus_QWIIC_Alphanumeric_Segment/InlandPlus_QWIIC_Alphanumeric_Segment.ino
+++ InlandPlus/InlandPlus_QWIIC_Alphanumeric_Segment/InlandPlus_QWIIC_Alphanumeric_Segment.ino
@@ -20,6 +20,28 @@
 #include <Wire.h>
 
 #include <SparkFun_Alphanumeric_Display.h>  //Click here to get the library: http://librarymanager/All#SparkFun_Qwiic_Alphanumeric_Display by SparkFun
+
+// Horizontal Side Segments
+#define HOR_TOP 'A'
+#define HOR_BOTTOM 'D'
+
+// Vertical Side Segments
+#define VER_BOTTOM_RIGHT 'C'
+#define VER_TOP_RIGHT 'B'
+#define VER_BOTTOM_LEFT 'E'
+#define VER_TOP_LEFT 'F'
+
+// Compass Direction Segments on the inside
+#define DIR_NN 'J'
+#define DIR_NE 'K'
+#define DIR_EE 'I'
+#define DIR_SE 'L'
+#define DIR_SS 'M'
+#define DIR_SW 'N'
+#define DIR_WW 'G'
+#define DIR_NW 'H'
+
+
 HT16K33 display;
 
 void setup()
@@ -38,9 +60,12 @@ void setup()
   display.clear(); 
   Serial.println("Display acknowledged.");
 
+
+
+
   //display.illuminateSegment('A', 0); // Top Horizontal
   //display.illuminateSegment('B', 0); // Top Right Vertical
-  //display.illuminateSegment('C', 0); // Lower Right Vertical
+  //display.illuminateSegment('C', 0); // Bottom Right Vertical
   //display.illuminateSegment('D', 0); // Bottom Horizontal
   //display.illuminateSegment('E', 0); // Bottom Left Vertical
   //display.illuminateSegment('F', 0); // Top Left Vertical
@@ -53,6 +78,7 @@ void setup()
   //display.illuminateSegment('M', 1); // SS Dir
   //display.illuminateSegment('N', 0); // SW Dir
 
+  /*
   display.illuminateSegment('A', 0);
   display.illuminateSegment('B', 0);
   display.illuminateSegment('C', 0);
@@ -69,6 +95,29 @@ void setup()
   display.illuminateSegment('N', 2);
   display.illuminateSegment('K', 2);
   display.illuminateSegment('L', 2);
+  */
+
+  // Outer segments that when all lit look like zero or letter 'O'
+  display.illuminateSegment(HOR_TOP, 0);
+  display.illuminateSegment(HOR_BOTTOM, 0);
+  display.illuminateSegment(VER_TOP_RIGHT, 0);
+  display.illuminateSegment(VER_BOTTOM_RIGHT, 0);
+  display.illuminateSegment(VER_TOP_LEFT, 0);
+  display.illuminateSegment(VER_BOTTOM_LEFT, 0);
+
+  // Inner horizontal and vertical segments that when lit look like plus sign
+  display.illuminateSegment(DIR_NN, 1);
+  display.illuminateSegment(DIR_EE, 1);
+  display.illuminateSegment(DIR_SS, 1);
+  display.illuminateSegment(DIR_WW, 1);
+
+  // Inner diagonal segments that when lit look like letter 'X'
+  display.illuminateSegment(DIR_NE, 2);
+  display.illuminateSegment(DIR_SE, 2);
+  display.illuminateSegment(DIR_SW, 2);
+  display.illuminateSegment(DIR_NW, 2);
+
+
 
   display.decimalOff();  //Turn decimals on
   display.decimalOn();   //Turn decimals off
