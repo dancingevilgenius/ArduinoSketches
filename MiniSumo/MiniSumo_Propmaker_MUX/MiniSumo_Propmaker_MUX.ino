6d97f71298f09587608114e39c2a6a78c8c60a4c
Carlos Garcia <dancingevilgenius@gmail.com>
2026-01-19 07:11:52 -0600

    Extracted new functions initDistanceSensors() and initMUX().

--- MiniSumo/MiniSumo_Propmaker_MUX/MiniSumo_Propmaker_MUX.ino
+++ MiniSumo/MiniSumo_Propmaker_MUX/MiniSumo_Propmaker_MUX.ino
@@ -6,12 +6,17 @@
 #include <Wire.h>
 #include <SparkFun_I2C_Mux_Arduino_Library.h> //Click here to get the library: http://librarymanager/All#SparkFun_I2C_Mux
 #include "SparkFun_VL53L1X.h" //Click here to get the library: http://librarymanager/All#SparkFun_VL53L1X
+
+
+
 #define NUM_DISTANCE_SENSORS 1
+#define NUM_LINE_SENSORS 1
 
 
 
 QWIICMUX myMux;
-SFEVL53L1X **distanceSensor; //Create pointer to a set of pointers to the sensor class
+SFEVL53L1X **distanceSensorArray; //Create pointer to a set of pointers to the sensor class
+void initMUX();
 
 #define INIT_SENSOR_SUCCESS 0
 #define INIT_SENSOR_FAIL 1
@@ -26,74 +31,85 @@ void setup()
   Wire.begin();
 
   setupSensors();
-
-
 }
 
 // Setup both line and distance sensors using QWIIC MUX
 void setupSensors() {
+
+
+  initMUX();
+
+  initDisanceSensors();
+
+}
+
+
+void initDisanceSensors(){
   //Create set of pointers to the class
-  distanceSensor = new SFEVL53L1X *[NUM_DISTANCE_SENSORS];
+  distanceSensorArray = new SFEVL53L1X *[NUM_DISTANCE_SENSORS];
 
   //Assign pointers to instances of the class
-  for (int x = 0; x < NUM_DISTANCE_SENSORS; x++)
-    distanceSensor[x] = new SFEVL53L1X(Wire);
-
-  if (myMux.begin(QWIIC_MUX_DEFAULT_ADDRESS, Wire) == false)
-  {
-    Serial.println("Mux not detected. Freezing...");
-    while (1)
-      ;
+  for (int x = 0; x < NUM_DISTANCE_SENSORS; x++){
+    distanceSensorArray[x] = new SFEVL53L1X(Wire);
   }
-  Serial.println("Mux detected");
+
 
   byte currentPortNumber = myMux.getPort();
   Serial.print("CurrentPort: ");
   Serial.println(currentPortNumber);
 
-  //Initialize all the sensors
+  //Initialize all the distance sensors
   bool initSuccess = true;
 
-  for (byte x = 0; x < NUM_DISTANCE_SENSORS; x++)
+  for (byte port = 0; port < NUM_DISTANCE_SENSORS; port++)
   {
-    myMux.setPort(x);
-    if (distanceSensor[x]->begin(Wire) == INIT_SENSOR_FAIL)
+    myMux.setPort(port);
+    if (distanceSensorArray[port]->begin(Wire) == INIT_SENSOR_FAIL)
     {
-      Serial.print("Sensor ");
-      Serial.print(x);
+      Serial.print("ERROR: Sensor ");
+      Serial.print(port);
       Serial.println(" did not begin! Check wiring");
       initSuccess = false;
     }
     else
     {
       //Configure each sensor
-      distanceSensor[x]->setIntermeasurementPeriod(180);
-      distanceSensor[x]->setDistanceModeLong();
-      distanceSensor[x]->startRanging(); //Write configuration bytes to initiate measurement
+      distanceSensorArray[port]->setIntermeasurementPeriod(180);
+      distanceSensorArray[port]->setDistanceModeLong();
+      distanceSensorArray[port]->startRanging(); //Write configuration bytes to initiate measurement
       Serial.print("Sensor ");
-      Serial.print(x);
+      Serial.print(port);
       Serial.println(" configured");
     }
   }
 
   if (initSuccess == false)
   {
-    Serial.print("Sensor initialization failed. exiting");
-    exit(1);
+    Serial.print("ERROR: Sensor initialization failed. exiting");
+    // @TODO Error display here
+    while(1)
+    ;
   }
 
-  Serial.println("Mux Shield online");
+  Serial.println("Distance sensors initialized.");  
 }
 
 void loop()
 {
+  loopSensors();
+
+  delay(180); //Wait for next reading
+}
+
+
+void loopSensors(){
   int distance[NUM_DISTANCE_SENSORS];
   float distanceFeet;
 
   for (byte x = 0; x < NUM_DISTANCE_SENSORS; x++)
   {
     myMux.setPort(x);                               //Tell mux to connect to this port, and this port only
-    distance[x] = distanceSensor[x]->getDistance(); //Get the result of the measurement from the sensor
+    distance[x] = distanceSensorArray[x]->getDistance(); //Get the result of the measurement from the sensor
 
     Serial.print("\tDistance");
     Serial.print(x);
@@ -108,7 +124,20 @@ void loop()
     Serial.print(distanceFeet, 2);
   }
 
-  Serial.println();
+  Serial.println();  
 
-  delay(180); //Wait for next reading
 }
+
+
+void initMUX(){
+  // Initialize MUX
+  if (myMux.begin(QWIIC_MUX_DEFAULT_ADDRESS, Wire) == false)
+  {
+    Serial.println("ERROR: Mux not detected. Freeze");
+    // @TODO Error display here    
+    while (1)
+      ;
+  }
+  Serial.println("Mux detected");
+}
+
