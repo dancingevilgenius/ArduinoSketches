db97e47434b09e18b98a4d4e205aa1388f484e80
Carlos Garcia <dancingevilgenius@gmail.com>
2026-05-09 02:50:09 -0500

    New tests including loopSimulateMiniSumo() which only shows 8x8 matrix.  First 6 rows are for opponent detection. Last 2 sensor rows are for edge detection.

--- ESP32/ESP32_QT_PY_qtmatrix_Tests/ESP32_QT_PY_qtmatrix_Tests.ino
+++ ESP32/ESP32_QT_PY_qtmatrix_Tests/ESP32_QT_PY_qtmatrix_Tests.ino
@@ -12,6 +12,21 @@ Adafruit_IS31FL3741_QT ledmatrix;
 // If colors appear wrong on matrix, try invoking constructor like so:
 // Adafruit_IS31FL3741_QT ledmatrix(IS3741_RBG);
 
+#define TOF_8x8_NUM_ROWS 8
+#define TOF_8x8_NUM_COLS 8
+
+uint8_t matrix[TOF_8x8_NUM_ROWS][TOF_8x8_NUM_COLS] = {
+  {0,0, 0, 0, 0, 0, 0 , 0},
+  {0,0, 0, 0, 0, 0, 0 , 0},
+  {0,0, 0, 0, 0, 0, 0 , 0},
+  {0,0, 0, 20, 20, 0, 0 , 0},
+  {0,0, 0, 0, 0, 0, 0 , 0},
+  {0,0, 0, 0, 0, 0, 0 , 0},
+  {10,10, 10, 10, 10, 10, 10 , 10},
+  {10,10, 10, 8, 10, 10, 10,10}
+  
+};
+
 // Some boards have just one I2C interface, but some have more...
 TwoWire *i2c = &Wire1; // e.g. change this to &Wire1 for QT Py RP2040
 
@@ -31,18 +46,140 @@ void setup() {
   i2c->setClock(800000);
 
   // Set brightness to max and bring controller out of shutdown state
-  ledmatrix.setLEDscaling(0x99); //0xFF
-  ledmatrix.setGlobalCurrent(0xFF); //0xFF
+  ledmatrix.setLEDscaling(0xAA); //0xFF
+  ledmatrix.setGlobalCurrent(0xCC); //0xFF
   Serial.print("Global current set to: ");
   Serial.println(ledmatrix.getGlobalCurrent());
   ledmatrix.enable(true); // bring out of shutdown
+
+  // Set all pixels to black 0x000
+  clearMatrix();
 }
 
 uint16_t hue_offset = 0;
 
 void loop() {
 
-  loopSameColor();
+  //loopSameColor();
+  //loopShowLastRow();
+  //loopShow8x8LastRow();
+  //loopShow8x8Gradients();
+  //loopReadFromMatrix();
+  loopSimulateMiniSumo();
+}
+
+void loopSimulateMiniSumo(){
+  uint16_t color565;
+  int d;
+  for (int y=0; y<TOF_8x8_NUM_ROWS ; y++) {
+    for (int x=0; x<TOF_8x8_NUM_COLS; x++) {
+      d = matrix[y][x];
+      if(y<TOF_8x8_NUM_ROWS -2 ){
+        
+        if(d > 0 && d < 30){
+          color565 = ledmatrix.color565(0, 150,0);
+        } else {
+          color565 = ledmatrix.color565(0, 0, 0);
+        }
+        ledmatrix.drawPixel(x, y, color565);
+      } else {
+        //color565 = ledmatrix.color565( matrix[y][x],0,0);
+        if(d < 10){
+          color565 = ledmatrix.color565( 100,0, 0);
+        } else {
+          color565 = ledmatrix.color565( 10,50,50);
+        }
+        ledmatrix.drawPixel(x, y, color565);
+      }
+    }
+  }
+  delay(2000);
+
+}
+
+void loopReadFromMatrix(){
+  uint8_t scale_factor = 18;
+  uint16_t color565;
+  for (int y=0; y<TOF_8x8_NUM_ROWS ; y++) {
+    for (int x=0; x<TOF_8x8_NUM_COLS; x++) {
+      if(y<TOF_8x8_NUM_ROWS -2 ){
+        if(matrix[y][x] >=20){
+          color565 = ledmatrix.color565(0, matrix[y][x],0);
+        } else {
+          color565 = ledmatrix.color565(0, 0, 0);
+        }
+        ledmatrix.drawPixel(x, y, color565);
+      } else {
+        //color565 = ledmatrix.color565( matrix[y][x],0,0);
+        color565 = ledmatrix.color565( 10,50,50);
+        ledmatrix.drawPixel(x, y, color565);
+      }
+    }
+  }
+  delay(2000);
+}
+
+
+void loopShow8x8Gradients(){
+  uint8_t scale_factor = 18;
+  for (int y=0; y<TOF_8x8_NUM_ROWS ; y++) {
+    for (int x=0; x<TOF_8x8_NUM_COLS; x++) {
+      uint16_t color565 = ledmatrix.color565(0, x*scale_factor,0);
+      if(y<TOF_8x8_NUM_ROWS -1 ){
+        ledmatrix.drawPixel(x, y, color565);
+      } else {
+        color565 = ledmatrix.color565( x*scale_factor,0,0);
+        ledmatrix.drawPixel(x, y, color565);
+      }
+    }
+  }
+  delay(2000);
+}
+
+void loopShow8x8LastRow(){
+
+  for (int y=0; y<TOF_8x8_NUM_ROWS ; y++) {
+    for (int x=0; x<TOF_8x8_NUM_COLS; x++) {
+      uint16_t color565 = ledmatrix.color565(0x00AA00);
+      if(y<TOF_8x8_NUM_ROWS -1){
+        ledmatrix.drawPixel(x, y, color565);
+      } else {
+        color565 = ledmatrix.color565(0xAA0000);
+        ledmatrix.drawPixel(x, y, color565);
+      }
+    }
+  }
+  delay(2000);
+
+}
+
+
+
+void clearMatrix(){
+
+
+  for (int y=0; y<ledmatrix.height(); y++) {
+    for (int x=0; x<ledmatrix.width(); x++) {
+      uint16_t color565 = ledmatrix.color565(0x000000);
+      ledmatrix.drawPixel(x, y, color565);
+    }
+  }
+}
+
+void loopShowLastRow(){
+
+  for (int y=0; y<ledmatrix.height(); y++) {
+    for (int x=0; x<ledmatrix.width(); x++) {
+      uint16_t color565 = ledmatrix.color565(0x00AA00);
+      if(y<ledmatrix.height() -1){
+        ledmatrix.drawPixel(x, y, color565);
+      } else {
+        color565 = ledmatrix.color565(0xAA0000);
+        ledmatrix.drawPixel(x, y, color565);
+      }
+    }
+  }
+  delay(2000);
 
 }
 
