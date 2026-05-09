fa358137eca9870f3a850e33dc5b69699dc7e217
Carlos Garcia <dancingevilgenius@gmail.com>
2026-05-09 01:17:16 -0500

    Added test to color last row different color (red). Added code to handle display of only 8x8 array.

---
+++ Rev3/R3_qtmatrix_Tests/R3_qtmatrix_Tests.ino
@@ -0,0 +1,150 @@
+// Rainbow swirl example for the Adafruit IS31FL3741 13x9 PWM RGB LED
+// Matrix Driver w/STEMMA QT / Qwiic connector. This is the simplest
+// version and should fit on small microcontrollers like Arduino Uno.
+// Tradeoff is that animation isn't always as smooth as seen in the
+// buffered example. Each LED changes state immediately when accessed,
+// there is no show() or display() function as with NeoPixels or some
+// OLED screens.
+
+#include <Adafruit_IS31FL3741.h>
+
+Adafruit_IS31FL3741_QT ledmatrix;
+// If colors appear wrong on matrix, try invoking constructor like so:
+// Adafruit_IS31FL3741_QT ledmatrix(IS3741_RBG);
+
+#define TOF_8x8_NUM_ROWS 8
+#define TOF_8x8_NUM_COLS 8
+
+// Some boards have just one I2C interface, but some have more...
+TwoWire *i2c = &Wire; // e.g. change this to &Wire1 for QT Py RP2040
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Adafruit QT RGB Matrix Simple RGB Swirl Test");
+
+  if (! ledmatrix.begin(IS3741_ADDR_DEFAULT, i2c)) {
+    Serial.println("IS41 not found");
+    while (1);
+  }
+
+  Serial.println("IS41 found!");
+
+  // By default the LED controller communicates over I2C at 400 KHz.
+  // Arduino Uno can usually do 800 KHz, and 32-bit microcontrollers 1 MHz.
+  i2c->setClock(800000);
+
+  // Set brightness to max and bring controller out of shutdown state
+  ledmatrix.setLEDscaling(0x44); //0xFF
+  ledmatrix.setGlobalCurrent(0xFF); //0xFF
+  Serial.print("Global current set to: ");
+  Serial.println(ledmatrix.getGlobalCurrent());
+  ledmatrix.enable(true); // bring out of shutdown
+
+  // Set all pixels to black 0x000
+  clearMatrix();
+}
+
+uint16_t hue_offset = 0;
+
+void loop() {
+
+  //loopSameColor();
+  //loopShowLastRow();
+  loopShow8x8LastRow();
+}
+
+void loopShow8x8LastRow(){
+
+  for (int y=0; y<TOF_8x8_NUM_ROWS ; y++) {
+    for (int x=0; x<TOF_8x8_NUM_COLS; x++) {
+      uint16_t color565 = ledmatrix.color565(0x00AA00);
+      if(y<TOF_8x8_NUM_ROWS -1){
+        ledmatrix.drawPixel(x, y, color565);
+      } else {
+        color565 = ledmatrix.color565(0xAA0000);
+        ledmatrix.drawPixel(x, y, color565);
+      }
+    }
+  }
+  delay(2000);
+
+}
+
+
+
+void clearMatrix(){
+
+
+  for (int y=0; y<ledmatrix.height(); y++) {
+    for (int x=0; x<ledmatrix.width(); x++) {
+      uint16_t color565 = ledmatrix.color565(0x000000);
+      ledmatrix.drawPixel(x, y, color565);
+    }
+  }
+}
+
+void loopShowLastRow(){
+
+  for (int y=0; y<ledmatrix.height(); y++) {
+    for (int x=0; x<ledmatrix.width(); x++) {
+      uint16_t color565 = ledmatrix.color565(0x00AA00);
+      if(y<ledmatrix.height() -1){
+        ledmatrix.drawPixel(x, y, color565);
+      } else {
+        color565 = ledmatrix.color565(0xAA0000);
+        ledmatrix.drawPixel(x, y, color565);
+      }
+    }
+  }
+  delay(2000);
+
+}
+
+void loopSameColor() {
+
+
+  for (int y=0; y<ledmatrix.height(); y++) {
+    for (int x=0; x<ledmatrix.width(); x++) {
+      uint16_t color565 = ledmatrix.color565(0xAA0000);
+      ledmatrix.drawPixel(x, y, color565);
+    }
+  }
+  delay(2000);
+
+
+  
+  for (int y=0; y<ledmatrix.height(); y++) {
+    for (int x=0; x<ledmatrix.width(); x++) {
+      uint16_t color565 = ledmatrix.color565(0x00AA00);
+      ledmatrix.drawPixel(x, y, color565);
+    }
+  }
+  delay(2000);
+
+  for (int y=0; y<ledmatrix.height(); y++) {
+    for (int x=0; x<ledmatrix.width(); x++) {
+      uint16_t color565 = ledmatrix.color565(0x0000AA);
+      ledmatrix.drawPixel(x, y, color565);
+    }
+  }
+  delay(2000);
+
+
+
+}
+
+void loopSwirlDemo(){
+  uint32_t i = 0;
+  for (int y=0; y<ledmatrix.height(); y++) {
+    for (int x=0; x<ledmatrix.width(); x++) {
+      uint32_t color888 = ledmatrix.ColorHSV(i * 65536 / 117 + hue_offset);
+      uint16_t color565 = ledmatrix.color565(color888);
+      ledmatrix.drawPixel(x, y, color565);
+      i++;
+    }
+  }
+
+  hue_offset += 256;
+
+  ledmatrix.setGlobalCurrent(hue_offset / 256); // Demonstrate global current
+}
