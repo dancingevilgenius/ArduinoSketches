cdcb083738f680b07b79823f91a2768ad4ea8185
Carlos Garcia <dancingevilgenius@gmail.com>
2026-05-24 07:39:59 -0500

    Added wait_ms param to blink(). #define ONE_SECOND.

--- ESP32/ESP32_Blink/ESP32_Blink.ino
+++ ESP32/ESP32_Blink/ESP32_Blink.ino
@@ -1,26 +1,29 @@
 //#define LED 2 // works with ESP32 DEV board, Acebott ESP32-Max
 #define LED 15  // ESP32-S2 Mini
+
+#define ONE_SEC 1000
 void setup() {
     Serial.begin(115200);
     pinMode(LED, OUTPUT);
 }
 
-void blink(int count, int interval){
+void blink(int count, int interval, int wait_ms){
     for(int i=0 ; i<count ; i++){
         digitalWrite(LED, HIGH);
         delay(interval);
         digitalWrite(LED, LOW);
         delay(interval);
     }
+
+    delay(wait_ms);
 }
 
 void loop() {
     Serial.println("blink cycle");
 
-    blink(1, 1000);
-    blink(3, 250);
-    delay(1000);
-    blink(5, 200);
-    delay(1000);
+    int wait_ms = ONE_SEC;
+    blink(1, 1000, wait_ms);
+    blink(3, 250, wait_ms);
+    blink(5, 200, wait_ms);
 
 }
