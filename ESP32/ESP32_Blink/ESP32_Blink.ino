993300343e3bd234ba89209ca7f495f4c624fb93
Carlos Garcia <dancingevilgenius@gmail.com>
2026-05-24 07:34:03 -0500

    Added Serial for logging. New blink() function.

--- ESP32/ESP32_Blink/ESP32_Blink.ino
+++ ESP32/ESP32_Blink/ESP32_Blink.ino
@@ -1,12 +1,26 @@
-#define LED 2 // works with ESP32 DEV board, Acebott ESP32-Max
-
+//#define LED 2 // works with ESP32 DEV board, Acebott ESP32-Max
+#define LED 15  // ESP32-S2 Mini
 void setup() {
+    Serial.begin(115200);
     pinMode(LED, OUTPUT);
 }
 
+void blink(int count, int interval){
+    for(int i=0 ; i<count ; i++){
+        digitalWrite(LED, HIGH);
+        delay(interval);
+        digitalWrite(LED, LOW);
+        delay(interval);
+    }
+}
+
 void loop() {
-    digitalWrite(LED, HIGH);
-    delay(700);
-    digitalWrite(LED, LOW);
-    delay(500);
+    Serial.println("blink cycle");
+
+    blink(1, 1000);
+    blink(3, 250);
+    delay(1000);
+    blink(5, 200);
+    delay(1000);
+
 }
